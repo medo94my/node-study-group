d4cb4a31c341a2993290905da13f91dea3533619
ahmed tawfik <medoroyalrma@gmail.com>
2021-12-13 10:33:05 +0000

    Initial commit

---
+++ input-output/index.js
@@ -0,0 +1,11 @@
+let {testNumber} = require('./game.js');
+
+process.stdout.write("I'm thinking of a number from 1 through 10. What do you think it is? \n(Write \"quit\" to give up.)\n\nIs the number ... ");
+
+let playGame = (userInput) => {
+  let input = userInput.toString().trim();
+console.log(input)
+	testNumber(input);
+};
+
+process.stdin.on('data',playGame)
